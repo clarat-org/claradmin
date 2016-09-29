b1d49b494c99ea8a99503bd512f1cead62143295
Konstantin K <konstantin.koss@gmail.com>
2016-09-29 11:16:02 +0200

    for clarat-org/clarat#928 - implement association filter in index

--- client/app/lib/settings.js
+++ client/app/lib/settings.js
@@ -3,7 +3,7 @@ export default {
     offer_translations: {
       fields: [
         'id', 'offer_id', 'locale', 'source', 'name',
-        'possibly_outdated'//, 'updated_at', 'offer_section'
+        'possibly_outdated', {offer: ['approved_at', 'created_by']}
       ],
       general_actions: [
         'index', 'export'
@@ -16,7 +16,7 @@ export default {
     organization_translations: {
       fields: [
         'id', 'organization_id', 'locale', 'source', 'description',
-        'possibly_outdated'//, 'updated_at', 'offer_section'
+        'possibly_outdated', 'organization.approved_at'
       ],
       general_actions: [
         'index', 'export'
