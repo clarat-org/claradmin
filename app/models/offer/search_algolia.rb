a81a09981ac30683284d4e6369d13c3c7759a7b3
allthegoldt <damjangoldt@Damjans-MacBook-Air.local>
2017-05-04 17:55:48 +0200

    removed keyword_string and tested

--- app/models/offer/search_algolia.rb
+++ app/models/offer/search_algolia.rb
@@ -10,12 +10,12 @@ module Offer::SearchAlgolia
     algoliasearch do
       I18n.available_locales.each do |locale|
         index = %w(
-          name description code_word next_steps keyword_string category_keywords
+          name description code_word next_steps category_keywords
           organization_names category_names stamp_family stamp_refugees tags
         )
         # :category_string,
         attributes = [:organization_count, :location_address, :location_name,
-                      :slug, :encounter, :keyword_string, :organization_names,
+                      :slug, :encounter, :organization_names,
                       :location_visible, :code_word]
         facets = [:_age_filters, :_language_filters, :_target_audience_filters,
                   :_exclusive_gender_filters, :section_identifier]
