09ee56f692d6e9b048b8a09d49fb18546f27122d
Nils Vollmer <nils_vollmer@gmx.de>
2017-05-08 13:21:12 +0200

    add definitions to algolia index

--- app/models/offer/search_algolia.rb
+++ app/models/offer/search_algolia.rb
@@ -10,10 +10,9 @@ module Offer::SearchAlgolia
     algoliasearch do
       I18n.available_locales.each do |locale|
         index = %w(
-          name description code_word next_steps category_keywords
+          name description code_word next_steps category_keywords definitions
           organization_names category_names stamp_family stamp_refugees tags
         )
-        # :category_string,
         attributes = [:organization_count, :location_address, :location_name,
                       :slug, :encounter, :organization_names,
                       :location_visible, :code_word]
