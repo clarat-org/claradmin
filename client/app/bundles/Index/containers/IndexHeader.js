f9a6e2d9ad5ebddb78bc3690f5e56039ff17541c
mokus80 <felhoesi@clarat.org>
2017-07-10 17:40:44 +0200

    parse filters and params from nested objects

--- client/app/bundles/Index/containers/IndexHeader.js
+++ client/app/bundles/Index/containers/IndexHeader.js
@@ -9,23 +9,29 @@ import settings from '../../../lib/settings'
 import IndexHeader from '../components/IndexHeader'
 
 const mapStateToProps = (state, ownProps) => {
-  const filters = toPairs(
+  const filterArray = toPairs(
     pickBy(ownProps.params, (value, key) => 
       key.substr(0, 7) == 'filters' && 
         lockedParamsHaveKey(key, ownProps.lockedParams) == false)
   )
+  const filters = toObject(filterArray)
+  const location = ownProps.location
   const plusButtonDisabled = ownProps.params.hasOwnProperty('filters[id]')
   const generalActions = settings.index[ownProps.model].general_actions
   const routes = generalRoutes(ownProps.model, ownProps.params).filter(route =>
     generalActions.includes(route.action)
   )
+  const filterKeys = filterArray.map(function(key) { return key[0] })
+
+  filterParams(ownProps.params)
   const params = ownProps.params
 
   return {
     params,
     filters,
     plusButtonDisabled,
-    routes
+    routes,
+    location
   }
 }
 
@@ -86,6 +92,44 @@ function searchString(model, params) {
   } else {
     return `?${encode(params)}`
   }
+}
+
+function toObject(filters) {
+  var filterArray = filters.map(function(filter) {
+    if (filter[0].includes("first")) {
+      const newKey = filter[0].replace("[first]", "")
+      return [ newKey, { "first": filter[1] } ]
+    } else if(filter[0].includes("second")) {
+      const newKey =  filter[0].replace("[second]", "")
+      return [ newKey, { "second":  filter[1] } ]
+    } else {
+      return [ filter[0], filter[1] ]
+    }
+  })
+  return filterArray
+}
+
+function filterParams(params) {
+  Object.keys(params).map(function(key) {
+    if (key.includes("first")) {
+      replaceKey(params, key, "[first]")
+    } else if(key.includes("second")) {
+      replaceKey(params, key, "[second]")
+    }
+    return params
+  })
+}
+
+function replaceKey(params, filterKey, objectKey) {
+  debugger
+  let newKey =  filterKey.replace(objectKey, '')
+  let newObjectKey = objectKey.replace('[', '').replace(']', '')
+  if(params.hasOwnProperty(newKey)) {
+    params[newKey][newObjectKey] = params[filterKey]
+  } else {
+    params[newKey] = { [newObjectKey] : params[filterKey] }
+  }
+  delete params[filterKey]
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(IndexHeader)
