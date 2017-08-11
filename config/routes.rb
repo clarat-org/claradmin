9be7ee6724aff1cc5f188c75fa32c1f7597aa74d
Konstantin K <konstantin.koss@gmail.com>
2017-08-11 15:59:26 +0200

    for clarat-org/clarat#1270 implement deletion for divisions

--- config/routes.rb
+++ config/routes.rb
@@ -96,7 +96,7 @@ Rails.application.routes.draw do
       api_resources :offers, only: [:index, :show]
       api_resources :locations
       api_resources :organizations
-      api_resources :divisions, only: [:show, :index, :create, :update]
+      api_resources :divisions
       api_resources :statistics, only: [:index]
       api_resources :users, only: [:index, :show, :update]
       api_resources :websites, only: [:index, :show, :create]
