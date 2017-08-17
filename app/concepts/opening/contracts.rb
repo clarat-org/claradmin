83f0d6e024d1123ae3a451a79a8484da2e1ce387
allthegoldt <damjangoldt@Damjans-MacBook-Air.local>
2017-08-17 13:07:20 +0200

    tests for Opening

--- app/concepts/opening/contracts.rb
+++ app/concepts/opening/contracts.rb
@@ -1,23 +1,17 @@
 # frozen_string_literal: true
 module Opening::Contracts
   class Create < Reform::Form
-
     property :day
     property :open
     property :close
-    property :sort_value
-    property :name
 
     validates :day, presence: true
     validate :unique_day_open_close
     validates :open, presence: true, if: :close
     validates :close, presence: true, if: :open
 
-    validates :sort_value, presence: true
-    validates :name, presence: true
-
     def unique_day_open_close
-      if Opening.where(day: day, open: open, close: close).count > 0
+      if Opening.where(day: day, open: open, close: close).count.positive?
         errors.add :day, I18n.t('errors.messages.taken')
         errors.add :open, I18n.t('errors.messages.taken')
         errors.add :close, I18n.t('errors.messages.taken')
--- app/concepts/opening/create.rb
+++ app/concepts/opening/create.rb
@@ -4,28 +4,27 @@ class Opening::Create < Trailblazer::Operation
   step Policy::Pundit(OpeningPolicy, :create?)
 
   step Contract::Build(constant: Opening::Contracts::Create)
+  step Contract::Validate()
   step :generate_name
   step :calculate_sort_value
-  step Contract::Validate()
   step Contract::Persist()
 
   def generate_name options
-    day = options['params']['data']['attributes']['day']
-    open = options['params']['data']['attributes']['open']
-    close = options['params']['data']['attributes']['close']
-    options['contract.default'].name = concat_day_and_times day, open, close
+    day = options['contract.default'].day
+    open = options['contract.default'].open
+    close = options['contract.default'].close
+    options['model'].name = concat_day_and_times day, open, close
   end
 
   # rails_admin can only sort by a single field, that's why we are creating an
   # imaginary time stamp that handles the sorting
   def calculate_sort_value options
-    day = options['params']['data']['attributes']['day']
-    open = options['params']['data']['attributes']['open']
-    close = options['params']['data']['attributes']['close']
+    day = options['contract.default'].day
+    open = options['contract.default'].open
+    close = options['contract.default'].close
     return nil unless day
-    # const DAYS = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
     dummy_time = dummy_time_for_day Opening::DAYS.index(day) + 1, open, close
-    options['contract.default'].sort_value = (dummy_time.to_f * 100).to_i
+    options['model'].sort_value = (dummy_time.to_f * 100).to_i
   end
 
   private
@@ -41,9 +40,10 @@ class Opening::Create < Trailblazer::Operation
   # generate imaginary timestamp for a specific day
   def dummy_time_for_day day_nr, open, close
     if open && close
-      hour = Time.parse(open).hour
-      min = Time.parse(open).min
-      sec = Time.parse(close).hour + Time.parse(close).min / 100.0
+      parsed_open = Time.zone.parse(open)
+      hour = parsed_open.hour
+      min = parsed_open.min
+      sec = Time.zone.parse(close).hour + Time.zone.parse(close).min / 100.0
     else
       hour = min = sec = 0
     end
