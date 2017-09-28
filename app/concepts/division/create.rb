815f2e292962213ab9678d23254dda77eabb8373
mokus80 <felhoesi@clarat.org>
2017-09-28 17:09:05 +0200

    all tests passing

--- app/concepts/division/create.rb
+++ app/concepts/division/create.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+require_relative './sync_organization.rb'
 class Division::Create < Trailblazer::Operation
+  include SyncOrganization
   include Assignable::CommonSideEffects::CreateNewAssignment
 
   step Model(::Division, :new)
-  step Policy::Pundit(DivisionPolicy, :create?)
+  step Policy::Pundit(PermissivePolicy, :create?)
 
   step Contract::Build(constant: Division::Contracts::Create)
   step Contract::Validate()
@@ -21,4 +23,5 @@ class Division::Create < Trailblazer::Operation
   }
   step Contract::Persist()
   step :create_initial_assignment!
+  step :syncronize_organization_approve_or_done_state
 end
