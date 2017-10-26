28a45b85e595412afcb0807799ebd461764b0fd5
allthegoldt <damjangoldt@Damjans-MacBook-Air.local>
2017-10-26 12:22:36 +0200

    sidekiq redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,12 @@
+require "fakeredis"
+require "sidekiq"
+
+redis_opts = {:url => "redis://127.0.0.1:6379/1", :namespace => "cms_queue"}
+# If fakeredis is loaded, use it explicitly
+redis_opts.merge!(:driver => Redis::Connection::Memory) if defined?(Redis::Connection::Memory)
+
 Sidekiq.configure_server do |config|
+  config.redis = redis_opts
   schedule_file = "config/worker_schedule.yml"
 
   if File.exists?(schedule_file)
