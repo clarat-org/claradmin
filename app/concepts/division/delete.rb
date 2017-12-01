d0a2fc908303d0d489f8ac410448955b5d2df7e1
mokus80 <felhoesi@clarat.org>
2017-12-01 15:27:37 +0100

    done

--- app/concepts/division/delete.rb
+++ app/concepts/division/delete.rb
@@ -1,19 +1,9 @@
 # frozen_string_literal: true
 
-class Division::Delete < Trailblazer::Operation
-  step :find_model
-  step Policy::Pundit(PermissivePolicy, :delete?)
-  step :abort_with_connected_offers
-  step :destroy!
-  step ::Lib::Macros::Live::SendDeletion()
-
-  def find_model(options, params:, model_class:, **)
-    options['model'] = model_class.find(params[:id])
-  end
-
-  def destroy!(_, model:, **)
-    model.destroy!
-  end
+class Division::Delete < Default::Delete
+  include SyncOrganization
+  step :abort_with_connected_offers, before: 'destroy_record'
+  step :syncronize_organization_approve_or_done_state, after: 'destroy_record'
 
   def abort_with_connected_offers(_, model:, **)
     model.offers.empty?
